4b9a6368adbb98ef5bdde8ce7b4b22e3fc040b43
Harold Thetiot <hthetiot@gmail.com>
2018-01-12 14:25:24 -0800

    fix linux platforms installer

--- platforms/linux/fpm.sh
+++ platforms/linux/fpm.sh
@@ -3,31 +3,38 @@
 # http://standards.freedesktop.org/desktop-entry-spec/latest/ar01s09.html
 # http://unix.stackexchange.com/questions/55214/how-to-override-the-gnome-3-ssh-handler/100736#100736
 # deb64
-export VERSION=0..1
-export NAME='popcorn'
+export VERSION=0.0.1
+export NAME='Popcorn'
 export VENDOR='Kaazing Corp'
-export DESCRIPTION='Montage PopCorn'
+export DESCRIPTION='Montage Popcorn'
 export URL='https://montagejs.github.io/popcorn/'
 export LICENCE='Kaazing Corp'
 export MAINTENER='Harold Thetiot <harold.thetiot@kaazing.com>'
 # TODO dynamic path
 BASEDIR=$(dirname "$0")
-export APP_PATH="$BASEDIR/../../"
+export APP_PATH="$BASEDIR/../.."
+export BUILD_PATH="$APP_PATH/build"
+export TMP_PATH="$BASEDIR/tmp"
+export WORKDIR_PATH="$APP_PATH/tmp"
+#clean
+rm -fr $BUILD_PATH/releases/*.deb
+rm -fr $BUILD_PATH/releases/*.rpm
 # start
-rm -fr tmp
+rm -fr $TMP_PATH
 #deb64
-mkdir -p tmp/usr/share/$NAME
-cp -r distro/* tmp/
-cp -r $APP_PATH/build/binaries/$VERSION/linux64/* tmp/usr/share/$NAME/
-fpm --verbose --epoch 0 -a amd64 -s dir -t deb -n "${NAME}" -v "${VERSION}" --vendor "${VENDOR}" --description "${DESCRIPTION}" --url "${URL}"  --license "${LICENCE}" --maintainer "${MAINTENER}" -p "${APP_PATH}/build/releases/${NAME}-${VERSION}.x86_64.deb" -C "${APP_PATH}/dist/linux/tmp" usr/share usr/bin
+mkdir -p $TMP_PATH/usr/share/$DIRNAME
+cp -r $BASEDIR/distro/* $TMP_PATH/
+cp -r $APP_PATH/build/binaries/$VERSION/linux64/* $TMP_PATH/usr/share/$NAME/
+fpm --verbose --epoch 0 -a amd64 -s dir -t deb -n "${NAME}" -v "${VERSION}" --vendor "${VENDOR}" --description "${DESCRIPTION}" --url "${URL}"  --license "${LICENCE}" --maintainer "${MAINTENER}" -p "${APP_PATH}/build/releases/${NAME}-${VERSION}.x86_64.deb" -C "${TMP_PATH}" usr/share usr/bin
 # rpm64
-rm -fr tmp
-mkdir tmp
-cp -r distro/* tmp/
-cp -r $APP_PATH/build/binaries/$VERSION/linux64/* tmp/usr/share/$NAME/
+rm -fr $TMP_PATH
+mkdir $TMP_PATH
+cp -r $BASEDIR/distro/* $TMP_PATH/
+cp -r $APP_PATH/build/binaries/$VERSION/linux64/* $TMP_PATH/usr/share/$NAME/
 # workdir
-rm -fr  $APP_PATH/tmp
-mkdir $APP_PATH/tmp
-fpm --verbose --workdir $APP_PATH/tmp --epoch 0 -a amd64 -s dir -t rpm -n "${NAME}" -v "${VERSION}" --vendor "${VENDOR}" --description "${DESCRIPTION}" --url "${URL}"  --license "${LICENCE}" --maintainer "${MAINTENER}" -p "${APP_PATH}/build/releases/${NAME}-${VERSION}.x86_64.rpm" -C "${APP_PATH}/dist/linux/tmp" usr/share usr/bin
+rm -fr  $WORKDIR_PATH
+mkdir $WORKDIR_PATH
+fpm --verbose --workdir $WORKDIR_PATH --epoch 0 -a amd64 -s dir -t rpm -n "${NAME}" -v "${VERSION}" --vendor "${VENDOR}" --description "${DESCRIPTION}" --url "${URL}"  --license "${LICENCE}" --maintainer "${MAINTENER}" -p "${APP_PATH}/build/releases/${NAME}-${VERSION}.x86_64.rpm" -C "${TMP_PATH}" usr/share usr/bin
 # end
-rm -fr tmp
+rm -fr $TMP_PATH
+rm -fr $WORKDIR_PATH
