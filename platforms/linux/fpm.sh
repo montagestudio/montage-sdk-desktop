0e1f2a0ca62b2cc824a88eea5f38f5f5ede5413b
Harold Thetiot <hthetiot@gmail.com>
2018-01-12 10:28:06 -0800

    crete fpm script to create deb and rpm pkg

--- platforms/linux/fpm.sh
+++ platforms/linux/fpm.sh
@@ -3,26 +3,31 @@
 # http://standards.freedesktop.org/desktop-entry-spec/latest/ar01s09.html
 # http://unix.stackexchange.com/questions/55214/how-to-override-the-gnome-3-ssh-handler/100736#100736
 # deb64
-export VERSION=0.5.3
-export NAME='myapp'
-export VENDOR='Montage SDK Dekstop'
-export DESCRIPTION=''
-export URL='https://www.montagestudio.com'
-export LICENCE='Kaazing Corp, all rights reserved.'
+export VERSION=0..1
+export NAME='popcorn'
+export VENDOR='Kaazing Corp'
+export DESCRIPTION='Montage PopCorn'
+export URL='https://montagejs.github.io/popcorn/'
+export LICENCE='Kaazing Corp'
 export MAINTENER='Harold Thetiot <harold.thetiot@kaazing.com>'
-export APP_PATH='/Users/hthetiot/projects/Montage/platforms/montage-sdk-desktop'
+# TODO dynamic path
+BASEDIR=$(dirname "$0")
+export APP_PATH="$BASEDIR/../../"
 # start
 rm -fr tmp
 #deb64
-mkdir tmp
+mkdir -p tmp/usr/share/$NAME
 cp -r distro/* tmp/
-cp -r $APP_PATH/build/binaries/$VERSION/linux64/* tmp/usr/share/myapp/
-fpm --epoch 0 -a amd64 -s dir -t deb -n "${NAME}" -v "${VERSION}" --vendor "${VENDOR}" --description "${DESCRIPTION}" --url "${URL}"  --license "${LICENCE}" --maintainer "${MAINTENER}" -p "${APP_PATH}/build/releases/${NAME}-${VERSION}.x86_64.deb" -C "${APP_PATH}/dist/linux/tmp" usr/share usr/bin
+cp -r $APP_PATH/build/binaries/$VERSION/linux64/* tmp/usr/share/$NAME/
+fpm --verbose --epoch 0 -a amd64 -s dir -t deb -n "${NAME}" -v "${VERSION}" --vendor "${VENDOR}" --description "${DESCRIPTION}" --url "${URL}"  --license "${LICENCE}" --maintainer "${MAINTENER}" -p "${APP_PATH}/build/releases/${NAME}-${VERSION}.x86_64.deb" -C "${APP_PATH}/dist/linux/tmp" usr/share usr/bin
 # rpm64
 rm -fr tmp
 mkdir tmp
 cp -r distro/* tmp/
-cp -r $APP_PATH/build/binaries/$VERSION/linux64/* tmp/usr/share/myapp/
-fpm --epoch 0 -a amd64 -s dir -t rpm -n "${NAME}" -v "${VERSION}" --vendor "${VENDOR}" --description "${DESCRIPTION}" --url "${URL}"  --license "${LICENCE}" --maintainer "${MAINTENER}" -p "${APP_PATH}/build/releases/${NAME}-${VERSION}.x86_64.rpm" -C "${APP_PATH}/dist/linux/tmp" usr/share usr/bin
+cp -r $APP_PATH/build/binaries/$VERSION/linux64/* tmp/usr/share/$NAME/
+# workdir
+rm -fr  $APP_PATH/tmp
+mkdir $APP_PATH/tmp
+fpm --verbose --workdir $APP_PATH/tmp --epoch 0 -a amd64 -s dir -t rpm -n "${NAME}" -v "${VERSION}" --vendor "${VENDOR}" --description "${DESCRIPTION}" --url "${URL}"  --license "${LICENCE}" --maintainer "${MAINTENER}" -p "${APP_PATH}/build/releases/${NAME}-${VERSION}.x86_64.rpm" -C "${APP_PATH}/dist/linux/tmp" usr/share usr/bin
 # end
 rm -fr tmp
