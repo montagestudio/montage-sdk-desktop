1c992cf0c44f6017bdee34e6dff24b9e83c13440
Harold Thetiot <hthetiot@gmail.com>
2018-03-14 16:22:21 -0700

    update platforms linux build script

--- platforms/linux/fpm.sh
+++ platforms/linux/fpm.sh
@@ -10,28 +10,47 @@ export DESCRIPTION='Montage PopCorn'
 export URL='https://montagejs.github.io/popcorn/'
 export LICENCE='Kaazing Corp'
 export MAINTENER='Harold Thetiot <harold.thetiot@kaazing.com>'
-# TODO dynamic path
-BASEDIR=$(dirname "$0")
+# Paths
+export BASEDIR="$( cd "$(dirname "$0")" ; pwd -P )"
 export APP_PATH="$BASEDIR/../.."
-export APP_PATH='/Users/harold/projects/Kaazing/demos/montage-sdk-desktop/'
 export BUILD_PATH="$APP_PATH/build"
+export TMP_PATH="$BASEDIR/tmp"
+export RPM_PATH="$BASEDIR/rpm"
 #clean
 rm -fr $BUILD_PATH/releases/*.deb
 rm -fr $BUILD_PATH/releases/*.rpm
 # start
-rm -fr tmp
+rm -fr $TMP_PATH
 #deb64
-mkdir tmp
-cp -r distro/* tmp/
-cp -r $APP_PATH/build/binaries/$VERSION/linux64/* tmp/usr/share/$NAME/
-fpm --verbose --epoch 0 -a amd64 -s dir -t deb -n "${NAME}" -v "${VERSION}" --vendor "${VENDOR}" --description "${DESCRIPTION}" --url "${URL}"  --license "${LICENCE}" --maintainer "${MAINTENER}" -p "${APP_PATH}/build/releases/${NAME}-${VERSION}.x86_64.deb" -C "${APP_PATH}/platforms/linux/tmp" usr/share usr/bin
+mkdir $TMP_PATH
+cp -r distro/* $TMP_PATH/
+cp -r $APP_PATH/build/binaries/$VERSION/linux64/* $TMP_PATH/usr/share/$NAME/
+fpm --deb-no-default-config-files --verbose --epoch 0 -a amd64 -s dir -t deb -n "${NAME}" -v "${VERSION}" --vendor "${VENDOR}" --description "${DESCRIPTION}" --url "${URL}"  --license "${LICENCE}" --maintainer "${MAINTENER}" -p "${APP_PATH}/build/releases/${NAME}-${VERSION}.x86_64.deb" -C "${TMP_PATH}" usr/share usr/bin
+# start
+rm -fr $TMP_PATH
+#deb32
+mkdir $TMP_PATH
+cp -r distro/* $TMP_PATH/
+cp -r $APP_PATH/build/binaries/$VERSION/linux32/* $TMP_PATH/usr/share/$NAME/
+fpm --deb-no-default-config-files --verbose --epoch 0 -a i386 -s dir -t deb -n "${NAME}" -v "${VERSION}" --vendor "${VENDOR}" --description "${DESCRIPTION}" --url "${URL}"  --license "${LICENCE}" --maintainer "${MAINTENER}" -p "${APP_PATH}/build/releases/${NAME}-${VERSION}.i386.deb" -C "${TMP_PATH}" usr/share usr/bin
+# start
+rm -fr $TMP_PATH
+rm -fr $RPM_PATH
 # rpm64
-rm -fr tmp
-mkdir tmp
-mkdir rpm
-cp -r distro/* tmp/
-cp -r $APP_PATH/build/binaries/$VERSION/linux64/* tmp/usr/share/$NAME/
-fpm --verbose --workdir $APP_PATH/platforms/linux/rpm --epoch 0 -a amd64 -s dir -t rpm -n "${NAME}" -v "${VERSION}" --vendor "${VENDOR}" --description "${DESCRIPTION}" --url "${URL}"  --license "${LICENCE}" --maintainer "${MAINTENER}" -p "${APP_PATH}/build/releases/${NAME}-${VERSION}.x86_64.rpm" -C "${APP_PATH}/platforms/linux/tmp" usr/share usr/bin
+mkdir $TMP_PATH
+mkdir $RPM_PATH
+cp -r distro/* $TMP_PATH/
+cp -r $APP_PATH/build/binaries/$VERSION/linux64/* $TMP_PATH/usr/share/$NAME/
+fpm --verbose --workdir "${RPM_PATH}" --epoch 0 -a amd64 -s dir -t rpm -n "${NAME}" -v "${VERSION}" --vendor "${VENDOR}" --description "${DESCRIPTION}" --url "${URL}"  --license "${LICENCE}" --maintainer "${MAINTENER}" -p "${APP_PATH}/build/releases/${NAME}-${VERSION}.x86_64.rpm" -C "${TMP_PATH}" usr/share usr/bin
+# start
+rm -fr $TMP_PATH
+rm -fr $RPM_PATH
+# rpm32
+mkdir $TMP_PATH
+mkdir $RPM_PATH
+cp -r distro/* $TMP_PATH/
+cp -r $APP_PATH/build/binaries/$VERSION/linux32/* $TMP_PATH/usr/share/$NAME/
+fpm --rpm-compression--verbose --workdir "${RPM_PATH}" --epoch 0 -a i386 -s dir -t rpm -n "${NAME}" -v "${VERSION}" --vendor "${VENDOR}" --description "${DESCRIPTION}" --url "${URL}"  --license "${LICENCE}" --maintainer "${MAINTENER}" -p "${APP_PATH}/build/releases/${NAME}-${VERSION}.i386.rpm" -C "${TMP_PATH}" usr/share usr/bin
 # end
-rm -fr tmp
-rm -fr rpm
+rm -fr $TMP_PATH
+rm -fr $RPM_PATH
