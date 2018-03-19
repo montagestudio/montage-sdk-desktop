d9d48bfc690c495549ea0b9fe431507680b71ba5
Harold Thetiot <hthetiot@gmail.com>
2018-03-19 12:32:26 -0700

    update release

---
+++ releases/release.sh
@@ -0,0 +1,10 @@
+
+export VERSION=0.0.3
+export NAME='Montage-PopCorn'
+
+ln -s $NAME-Install-$VERSION-x64.exe $NAME-Install-x64.exe 
+ln -s $NAME-Install-$VERSION-x86.exe $NAME-Install-x86.exe 
+ln -s $NAME-Setup-$VERSION.dmg $NAME-Setup.dmg 
+ln -s $NAME-$VERSION.x86_64.deb $NAME.x86_64.deb 
+ln -s $NAME-$VERSION.x86_64.rpm $NAME.x86_64.rpm 
+ln -s $NAME-$VERSION.i386.deb $NAME.i386.deb 
